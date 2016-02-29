4dab2123d72d992236e519e08afcced9a218a189
任宇龙 <yulong.ryl@alibaba-inc.com>
2016-02-29 20:29:34 +0800

    添加threeSum

--- src/main/java/com/ryl/learn/lecode/MediumMain.java
+++ src/main/java/com/ryl/learn/lecode/MediumMain.java
@@ -1,12 +1,8 @@
 package com.ryl.learn.lecode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
- *
  * 题目答案都能搜到
  * Created by renyulong on 16/2/3.
  */
@@ -18,9 +14,82 @@ public class MediumMain {
         //-1, 0, 1, 2, -1, -4
         System.out.println(main.threeSum(new int[]{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}));
 
-        System.out.println(main.intToRoman(123));
-        System.out.println(main.int2Roman(123));
+//        System.out.println(main.intToRoman(123));
+//        System.out.println(main.int2Roman(123));
     }
+
+    //http://blog.csdn.net/li4951/article/details/8693212
+    public List<List<Integer>> threeSumW(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+
+                        int max = nums[i] > nums[j] ? nums[k] > nums[i] ? nums[k] : nums[i] : nums[k] > nums[j] ? nums[k] : nums[j];
+                        int min = nums[i] < nums[j] ? nums[k] < nums[i] ? nums[k] : nums[i] : nums[k] < nums[j] ? nums[k] : nums[j];
+                        int mid = 0 - max - min;
+
+                        if (set.contains(min - mid - max)) {
+                            break;
+                        }
+
+                        List<Integer> subList = new ArrayList<Integer>(3);
+                        subList.add(min);
+                        subList.add(mid);
+                        subList.add(max);
+                        res.add(subList);
+
+
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private List<List<Integer>> list;
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        list = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for (int i = 0; i <= nums.length - 3; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            judgeAndPut(nums, i, i + 1, nums.length - 1);
+        }
+        return list;
+    }
+
+
+    private void judgeAndPut(int[] num, int i, int p, int q) {
+        while (p < q) {
+            if (num[p] + num[q] + num[i] < 0) {
+                p++;
+            } else if (num[p] + num[q] + num[i] > 0) {
+                q--;
+            } else {
+                List<Integer> tmpList = new ArrayList<Integer>();
+                tmpList.add(num[i]);
+                tmpList.add(num[p]);
+                tmpList.add(num[q]);
+                list.add(tmpList);
+                p++;
+                q--;
+            }
+            while (p < q && num[p] == num[p - 1]) {
+                p++;
+            }
+            while (p < q && num[q] == num[q + 1]) {
+                q--;
+            }
+        }
+
+    }
+
 
     //http://blog.csdn.net/ljiabin/article/details/39968583
     public String int2Roman(int num) {
@@ -51,38 +120,6 @@ public class MediumMain {
             }
         }
         return str;
-    }
-
-    //http://blog.csdn.net/li4951/article/details/8693212
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        Set<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-
-                        int max = nums[i] > nums[j] ? nums[k] > nums[i] ? nums[k] : nums[i] : nums[k] > nums[j] ? nums[k] : nums[j];
-                        int min = nums[i] < nums[j] ? nums[k] < nums[i] ? nums[k] : nums[i] : nums[k] < nums[j] ? nums[k] : nums[j];
-                        int mid = 0 - max - min;
-
-                        if (set.contains(min - mid - max)) {
-                            break;
-                        }
-
-                        List<Integer> subList = new ArrayList<Integer>(3);
-                        subList.add(min);
-                        subList.add(mid);
-                        subList.add(max);
-                        res.add(subList);
-
-
-                        break;
-                    }
-                }
-            }
-        }
-        return res;
     }
 
     public int threeSumClosest(int[] nums, int target) {

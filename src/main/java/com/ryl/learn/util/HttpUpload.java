dfdc056c3e98f892f7c15fdebbfcd454cb5b1409
任宇龙 <yulong.ryl@alibaba-inc.com>
2016-07-08 14:53:19 +0800

    添加HttpUpload 异步http client调用

--- src/main/java/com/ryl/learn/util/HttpUpload.java
+++ src/main/java/com/ryl/learn/util/HttpUpload.java
@@ -1,26 +1,25 @@
 package com.ryl.learn.util;
 
-import com.google.common.base.Charsets;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.nio.client.HttpAsyncClient;
 import org.apache.http.util.EntityUtils;
 import org.joda.time.DateTime;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -38,16 +37,17 @@ public class HttpUpload {
 
 
     public static void main(String[] args) {
-        uploadApk();
+//        uploadApk();
 //        uploadImage();
-//        sendHttp();
+        sendHttp();
     }
 
     public static void sendHttp() {
-        ExecutorService service = Executors.newFixedThreadPool(1);
-        String url = "http://www.clctrip.com/app/user/send_code.json";
-        for (int i = 0; i < 1; i++) {
-            service.submit(new i2doWorker(url));
+        ExecutorService service = Executors.newFixedThreadPool(9);
+        for (int i = 0; i < 3; i++) {
+            service.submit(new i2doWorker("http://wan.sogou.com/ajax/i2.do?_=1467959528114"));
+            service.submit(new i2doWorker("http://wan.sogou.com/msg/unreadnum/85646042?callback=jQuery1113009500109593701245_1467960097997&_=1467960097998"));
+            service.submit(new i2doWorker("http://wan.sogou.com/getUserAntiAddiction.do?1=1"));
         }
     }
 
@@ -81,17 +81,43 @@ public class HttpUpload {
 
         @Override
         public void run() {
-            HttpClient client = HttpClientBuilder.create().build();
+//            HttpClient client = HttpClientBuilder.create().build();
+            CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
+            client.start();
             while (true) {
                 try {
                     String current = new DateTime().toString(format);
-                    HttpPost post = new HttpPost(url);
-                    List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-                    nvps.add(new BasicNameValuePair("to", "15210830381"));
-                    post.setEntity(new UrlEncodedFormEntity(nvps, Charsets.UTF_8));
-                    HttpResponse response = client.execute(post);
-                    System.out.println(current + " " + Thread.currentThread().getName() + " " + EntityUtils.toString(response.getEntity()));
-                } catch (IOException e) {
+                    HttpGet get = new HttpGet(url + "&t=" + System.nanoTime());
+                    get.addHeader("cookie", "SUV=004F7D8DB65CFD03577C64796F108139; IPLOC=CN3301; JSESSIONID=aaapRZJZYX1LthVxv69wv; IESESSION=alive; tencentSig=5346518016; pgv_pvi=8242046976; pgv_si=s6700715008; GOTO=; _ga=GA1.2.791106921.1467774573; IPLOC=CN1101; _qddab=3-ib1dbf.iqar31xl; countdate=1|1467801812; email=eppe333%40sogou.com; ppinf=5|1467858213|1469067813|dHJ1c3Q6MToxfGNsaWVudGlkOjQ6MTEwMHx1bmlxbmFtZTowOnxjcnQ6MTA6MTQ2Nzg1ODIxM3xyZWZuaWNrOjA6fHVzZXJpZDoxNzplcHBlMjIyQHNvZ291LmNvbXw; pprdig=ALSd6GQgQCVSOvs65qdlddkvUx4ZtG0gtxihR2z2C3MGKmd-3gLTMad2_z_GdTGH6Hq-FNwNo6Yrj1jsm1jfR3tyaeHA7I_KQzZyoDwUCbUPO6GV2YlKmvLV692TAmJGP7U8UXmoFf01u68kAtVlOO6sStTHM3SO6J3O0DzqnQo; CXID=78B03FEC87FDF83DF09FC8A6BB8480CB; ad=Tyllllllll2gKAAAlllllVNM33wlllllHO8dlkllll9lllllxCxlw@@@@@@@@@@@; SUID=03FD5CB65412940A00000000577CC9E4; swfLayer=1; ppmdig=1467959249000000a75c14664d14701486b920be93bf553e; JSESSIONID=aaapRZJZYX1LthVxv69wv; source=0010000100000; hostid=85646042");
+//                    HttpPost post = new HttpPost(url);
+//                    List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+//                    nvps.add(new BasicNameValuePair("to", "15210830381"));
+//                    post.setEntity(new UrlEncodedFormEntity(nvps, Charsets.UTF_8));
+                    client.execute(get, new FutureCallback<HttpResponse>() {
+                        @Override
+                        public void completed(HttpResponse response) {
+                            int num = count.incrementAndGet();
+                            if(num % 1000 == 0) {
+                                try {
+                                    System.out.println(current + " " + Thread.currentThread().getName() + " " + num + " " + EntityUtils.toString(response.getEntity()));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void failed(Exception e) {
+                            System.err.println("failed" + e);
+                        }
+
+                        @Override
+                        public void cancelled() {
+
+                        }
+                    });
+                } catch (Exception e) {
+                    System.err.println(e);
                     e.printStackTrace();
                 }
             }

929869f94704d95a88a9b8ae3858a130549affba
任宇龙 <yulong.ryl@alibaba-inc.com>
2016-09-06 18:16:12 +0800

    easy code

--- src/main/java/com/ryl/learn/lecode/EasyImpl.java
+++ src/main/java/com/ryl/learn/lecode/EasyImpl.java
@@ -81,6 +81,34 @@ public class EasyImpl {
     }
 
     /**
+     * 157. Read N Characters Given Read4
+     * The API: int read4(char[] buf) reads 4 characters at a time from a file.
+     * The return value is the actual number of characters read.
+     * For example, it returns 3 if there is only 3 characters left in the file.
+     * By using the read4 API, implement the function int read(char[] buf, int n) that reads n characters from the file.
+     *
+     * @param buf char[]
+     * @param n   int
+     * @return int 实际读取字符数
+     */
+    public int read(char[] buf, int n) {
+        int read = 0;
+        char[] temp = new char[4];
+        while (read < n) {
+            int r = read4(temp);
+            for (int i = 0; read < n && i < r; i++) {
+                buf[read++] = temp[i];
+            }
+            if (r < 4) break;
+        }
+        return read;
+    }
+
+    private int read4(char[] buf) {
+        return 3;
+    }
+
+    /**
      * 276. Paint Fence
      * There is a fence with n posts, each post can be painted with one of the k colors.
      * You have to paint all the posts such that no more than two adjacent fence posts have the same color.
--- src/main/java/com/ryl/learn/lecode/TwoSum.java
+++ src/main/java/com/ryl/learn/lecode/TwoSum.java
@@ -1,11 +1,54 @@
 package com.ryl.learn.lecode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 170. Two Sum III - Data structure design
+ * Design and implement a TwoSum class. It should support the following operations: add and find.
  *
+ * add - Add the number to an internal data structure.
+ * find - Find if there exists any pair of numbers which sum is equal to the value.
  *
+ * add(1); add(3); add(5);
+ * find(4) -> true
+ * find(7) -> false
  *
  * Created on 16/9/6 下午4:23.
  */
 public class TwoSum {
+
+    private Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+
+    public void add(int number) {
+        Integer value = map.get(number);
+        if(value == null) {
+            map.put(number,1);
+        } else {
+            map.put(number,++value);
+        }
+    }
+
+    public boolean find(int value) {
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if(value != 2 * entry.getKey() && map.containsKey(value - entry.getKey())) {
+                return true;
+            }
+            if(value == 2 * entry.getKey() && entry.getValue() > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TwoSum sum = new TwoSum();
+        sum.add(1);
+        sum.add(3);
+        sum.add(5);
+        System.out.println(sum.find(4));
+        System.out.println(sum.find(7));
+        System.out.println(sum.find(6));
+        System.out.println(sum.find(10));
+    }
 }

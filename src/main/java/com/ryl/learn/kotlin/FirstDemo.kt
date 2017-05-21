decfa816ed02929d2a8cacf5232466ed2b1fd7b1
任宇龙 <yulong.ryl@alibaba-inc.com>
2017-05-21 15:35:40 +0800

    添加kotlin  demo

---
+++ src/main/java/com/ryl/learn/kotlin/FirstDemo.kt
@@ -0,0 +1,60 @@
+package com.ryl.learn.kotlin
+
+/**
+ * Created on 17/5/19 18:19.
+ */
+open class FirstDemo {
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sum2(a: Int, b: Int) = a + b
+
+    fun printSum(a: Int, b: Int): Unit {
+        println("sum of $a + $b = ${a + b}")
+    }
+    
+    
+}
+
+data class Customer (val id: Int, val name : String)
+
+fun String.spaceToCamel() : String {
+    var result : String = ""
+    for(word in this.split(" ")) {
+        print(word)
+    }
+    return result
+}
+
+
+fun main(args: Array<String>) {
+    println("Hello World")
+    val a: Int = 1
+    val b = 2
+    val c: Int
+    c = 3
+    println("$a $b $c")
+
+    var x = 4
+    x += 1
+    println(x)
+    
+    val s1 = "a is $a"
+    val s2 = "${s1.replace("is","was")}, but now is $a"
+    println("s1=$s1 s2=$s2")
+    
+    var items = listOf("apple","banana","kiwi")
+    for(item in items) {
+        println(item)
+    }
+    for(index in items.indices) {
+        println("item at $index is ${items[index]}")
+    }
+    
+    println(Customer(23, "zhangsan").toString())
+    
+    println("my name is zhangsan".spaceToCamel())
+    
+}

298217ebd08b06864268b046f641b96d9ef25df1
任宇龙 <yulong.ryl@alibaba-inc.com>
2017-09-22 13:55:38 +0800

    修改car excel处理类

--- src/main/java/com/ryl/learn/excel/CarCsvUtils.java
+++ src/main/java/com/ryl/learn/excel/CarCsvUtils.java
@@ -3,9 +3,11 @@ package com.ryl.learn.excel;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -13,6 +15,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,7 +27,7 @@ public class CarCsvUtils {
     
     
     public static void main(String[] args) throws Exception {
-        
+
 //        getFeeDetail("9002");
 //        shenzhouExcel();
         shouqiExcel();
@@ -33,28 +36,29 @@ public class CarCsvUtils {
     
     /**
      * 财务每月数据导出
-     select a.*, b.fee_detail from (
-        select * from car_order where gmt_create >= '2017-07-01 00:00:00' and gmt_create <= '2017-07-31 23:59:59' 
-        and cp_code = 9002 and status in (106,112) and amap_ride_type <> 'gd_taxi') a 
-        left join car_order_bill b on (a.amap_order_id = b.amap_order_id)
+     * select a.*, b.fee_detail from (
+     * select * from car_order where gmt_create >= '2017-07-01 00:00:00' and gmt_create <= '2017-07-31 23:59:59'
+     * and cp_code = 9002 and status in (106,112) and amap_ride_type <> 'gd_taxi') a
+     * left join car_order_bill b on (a.amap_order_id = b.amap_order_id)
      */
     
     
     private static void shenzhouExcel() throws Exception {
         String cpCode = "9003";
-        File fileOut = new File("/Users/alibaba/logs/car_shenzhou_201707.xlsx");
+        File fileOut = new File("/Users/alibaba/Downloads/car_shenzhou_201708.xlsx");
         
         Workbook wbOut = new XSSFWorkbook();
         Sheet sheetOut = wbOut.createSheet("sheet1");
         
-        File excel = new File("/Users/alibaba/Downloads/2017-08-10-11-13-48_EXPORT_XLSX_905978_496_0.xlsx");
+        File excel = new File("/Users/alibaba/Downloads/201708月神州对账-20170918.xlsx");
         FileInputStream fis = new FileInputStream(excel);
         Workbook wb = WorkbookFactory.create(fis);
         Sheet sheet = wb.getSheetAt(0);
         Row row = sheet.getRow(1);
         int rowNum = sheet.getPhysicalNumberOfRows();
         int colNum = row.getPhysicalNumberOfCells();
-        String[] feeDetailCols = new String[]{"停车费", "时长费", "抹零", "路桥费", "违约金", "清洁费", "里程费", "出城费", "起租价", "远途费", "其他费用"};
+        String[] feeDetailCols =
+                new String[]{"停车费", "时长费", "抹零", "路桥费", "违约金", "里程费", "出城费", "起租价", "其他费用", "远途费"};
         
         int outRowNum = 0;
         Row rowOut = sheetOut.createRow(outRowNum);
@@ -71,15 +75,17 @@ public class CarCsvUtils {
         String[] values = new String[colNum];
         for (int r = 2; r <= rowNum; r++) {
             row = sheet.getRow(r);
+            if (row == null) break;
             String cpCodeExcel = String.valueOf(row.getCell(7));
-            if (!cpCode.equals(cpCodeExcel)) continue;
+            if (!cpCodeExcel.contains(cpCode)) continue;
             outRowNum++;
             rowOut = sheetOut.createRow(outRowNum);
             for (int i = 1; i <= colNum; i++) {
-                String value = String.valueOf(row.getCell(i));
-                Cell cell = rowOut.createCell(i - 1);
-                cell.setCellValue(value);
+                Cell cellIn = row.getCell(i);
+                String value = String.valueOf(cellIn);
                 values[i - 1] = value;
+                Cell cell = rowOut.createCell(i - 1);
+                copyCell(cellIn, cell);
             }
             //处理feeDetail
             String feeDetail = values[colNum - 1];
@@ -98,6 +104,7 @@ public class CarCsvUtils {
                         value = "0";
                     }
                     Cell cell = rowOut.createCell(colNum + i);
+                    cell.setCellType(CellType.STRING);
                     cell.setCellValue(value);
                 }
             }
@@ -111,20 +118,19 @@ public class CarCsvUtils {
     
     private static void shouqiExcel() throws Exception {
         String cpCode = "9002";
-        File fileOut = new File("/Users/alibaba/logs/car_shouqi_7.xlsx");
+        File fileOut = new File("/Users/alibaba/Downloads/car_shouqi_201708.xlsx");
         
         Workbook wbOut = new XSSFWorkbook();
         Sheet sheetOut = wbOut.createSheet("sheet1");
         
-        File excel = new File("/Users/alibaba/Downloads/2017-08-14-19-04-08_EXPORT_XLSX_912874_791_0.xlsx");
+        File excel = new File("/Users/alibaba/Downloads/201708月首汽对账-20170920.xlsx");
         FileInputStream fis = new FileInputStream(excel);
         Workbook wb = WorkbookFactory.create(fis);
         Sheet sheet = wb.getSheetAt(0);
         Row row = sheet.getRow(1);
         int rowNum = sheet.getPhysicalNumberOfRows();
         int colNum = row.getPhysicalNumberOfCells();
-        String[] shouqi = new String[]{"基础价格", "高峰时长费", "抹零", "夜间里程费", "空驶费", "超里程费用", "高速服务费", "超时长费用"};
-        shouqi = new String[]{"基础价格", "高峰时长费", "停车费", "抹零", "夜间里程费", "空驶费", "超里程费用", "高速服务费", "超时长费用", "夜间时长费用"};
+        String[] shouqi = new String[]{"基础价格", "高峰时长费", "停车费", "抹零", "夜间里程费", "空驶费", "超里程费用", "高速服务费", "高峰里程费用", "等待费用", "超时长费用", "夜间时长费用"};
         int outRowNum = 0;
         Row rowOut = sheetOut.createRow(outRowNum);
         for (int i = 1; i <= colNum; i++) {
@@ -143,19 +149,25 @@ public class CarCsvUtils {
         for (int r = 2; r <= rowNum; r++) {
             row = sheet.getRow(r);
             String cpCodeExcel = String.valueOf(row.getCell(7));
-            if (!cpCode.equals(cpCodeExcel)) continue;
+            if (!cpCodeExcel.contains(cpCode)) continue;
             outRowNum++;
             rowOut = sheetOut.createRow(outRowNum);
             for (int i = 1; i <= colNum; i++) {
-                String value = String.valueOf(row.getCell(i));
-                Cell cell = rowOut.createCell(i - 1);
-                cell.setCellValue(value);
+                Cell cellIn = row.getCell(i);
+                String value = String.valueOf(cellIn);
                 values[i - 1] = value;
+                if(cellIn == null) {
+                    rowOut.createCell(i - 1);
+                    continue;
+                }
+                Cell cell = rowOut.createCell(i - 1, cellIn.getCellTypeEnum());
+                copyCell(cellIn, cell);
             }
             //处理feeDetail
             String feeDetail = values[colNum - 1];
             if (!Strings.isNullOrEmpty(feeDetail)) {
                 JSONArray jsonArray = JSON.parseArray(feeDetail);
+                if (jsonArray == null) continue;
                 Map<String, String> feeDetailMap = Maps.newHashMap();
                 for (int i = 0; i < jsonArray.size(); i++) {
                     JSONObject object = jsonArray.getJSONObject(i);
@@ -182,7 +194,7 @@ public class CarCsvUtils {
     }
     
     private static void getFeeDetail(String cpCode) throws Exception {
-        File excel = new File("/Users/alibaba/Downloads/2017-08-14-19-04-08_EXPORT_XLSX_912874_791_0.xlsx");
+        File excel = new File("/Users/alibaba/Downloads/201708月首汽对账-20170920.xlsx");
         FileInputStream fis = new FileInputStream(excel);
         Workbook wb = WorkbookFactory.create(fis);
         Sheet sheet = wb.getSheetAt(0);
@@ -192,11 +204,17 @@ public class CarCsvUtils {
         Set<String> feeDetailCols = Sets.newHashSet();
         for (int r = 2; r <= rowNum; r++) {
             row = sheet.getRow(r);
-            String cpCodeExcel = String.valueOf(row.getCell(7));
-            if (!cpCode.equals(cpCodeExcel)) continue;
+            if (row == null) break;
+            Cell cpCodeCell = row.getCell(7);
+            if (cpCodeCell == null) break;
+            String cpCodeExcel = String.valueOf(cpCodeCell);
+            if (!cpCodeExcel.contains(cpCode)) continue;
             //处理feeDetail
-            String feeDetail = String.valueOf(row.getCell(colNum));
+            Cell cell = row.getCell(colNum);
+            if (cell == null) continue;
+            String feeDetail = String.valueOf(cell);
             if (!Strings.isNullOrEmpty(feeDetail)) {
+                System.out.println(r + " " + feeDetail);
                 JSONArray jsonArray = JSON.parseArray(feeDetail);
                 for (int i = 0; i < jsonArray.size(); i++) {
                     JSONObject object = jsonArray.getJSONObject(i);
@@ -206,7 +224,31 @@ public class CarCsvUtils {
             }
         }
         System.out.println(feeDetailCols);
+        System.out.println("\"" + Joiner.on("\",\"").join(feeDetailCols) + "\"");
     }
     
-    
+    private static void copyCell(Cell sourceCell, Cell targetCell) {
+        switch (sourceCell.getCellTypeEnum()) {
+            case STRING:
+                targetCell.setCellValue(sourceCell.getRichStringCellValue());
+                break;
+            case NUMERIC:
+                targetCell.setCellValue(sourceCell.getNumericCellValue());
+                break;
+            case BLANK:
+                targetCell.setCellType(CellType.BLANK);
+                break;
+            case BOOLEAN:
+                targetCell.setCellValue(sourceCell.getBooleanCellValue());
+                break;
+            case ERROR:
+                targetCell.setCellErrorValue(sourceCell.getErrorCellValue());
+                break;
+            case FORMULA:
+                targetCell.setCellFormula(sourceCell.getCellFormula());
+                break;
+            default:
+                break;
+        }
+    }
 }
